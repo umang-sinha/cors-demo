039fb429a921c00aad46de48eaa1a0ca82b1aa99
umangsinha12@gmail.com <umangsinha12@gmail.com>
2024-02-11 07:19:06 +0530

    feat: cors demo client

---
+++ client/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = 4000;
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
